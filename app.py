1997a3c53c7c4016f80436127fd97638007f952f
arnold_j <arnold_j@lanl.gov>
2016-11-02 14:58:36 -0600

    Add initial files
    These files originall came from work done on arnold/service-experiment. They
    are just the front piece. There are other scripts that handle the worker logic;
    may be added later.

---
+++ app.py
@@ -0,0 +1,31 @@
+import json
+from flask import Flask, jsonify, \
+                  render_template, request
+from client import send_message, query_job, submit_job
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<service>', methods=['POST'])
+def post_job():
+    message = request.get_json()
+    job_id = send_message(message, service)
+    submit_job(job_id, service)
+    response = {'jobId': job_id, 'status': 'submitted'}
+    return jsonify(**response)
+
+
+@app.route('/<service>/jobs/<job_id>', methods=['GET'])
+def query_job():
+    status = query_job(job_id)
+    response = {'jobId': job_id, 'status': status}
+    return jsonify(**response)
+
+
+if __name__ == '__main__':
+#    app.run(host='0.0.0.0', debug=True)
+    app.run(host='127.0.0.1', debug=True)
---
+++ client.py
@@ -0,0 +1,29 @@
+import boto3
+import json
+
+def send_message(msg, service):
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName='service-experiment')
+    resp = queue.send_message(MessageBody=json.dumps(msg),
+                              MessageAttributes={'Service': {'StringValue': service, 
+                                                             'DataType': 'String'}})
+    return resp.get('MessageId')
+
+
+def submit_job(job_id, service):
+    db = boto3.resource('dynamodb')
+    table = db.Table('service-experiment')
+    table.put_item(Item={
+        'job_id': job_id,
+        'service': service,
+        'result': None,
+        'status': 'submitted'
+    })
+
+
+def query_job(job_id):
+    db = boto3.resource('dynamodb')
+    table = db.Table('service-experiment')
+    response = table.get_item(Key={'job_id': job_id})
+    job = response['Item']
+    return job['status']
