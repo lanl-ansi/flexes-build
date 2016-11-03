d11a67aaf8ae21f1fc344d12be57f072b26e0c82
James Arnold <arnold_j@lanl.gov>
2016-11-03 11:50:21 -0600

    Rough out popecon interface

--- static/js/init.js
+++ static/js/init.js
@@ -1,3 +1,21 @@
+var jobId;
+
 $(function () {
   $.material.init();
+});
+
+$('body').on('click', '#PopEconSubmit', function () {
+  var input = $('#PopEconInput').val();
+  $.post('./popecon', input, function(response) {
+    jobId = response.jobId;
+    $('#PopEconQueryDiv').show(1000);
+  });
+});
+
+$('body').on('click', '#PopEconQueryBtn', function () {
+  var url = './popecon/jobs/' + jobId;
+  $.get(url, function(response) {
+    $('#PopEconQueryResult').empty();
+    $('#PopEconQueryResult').text(JSON.stringify(response));
+  })
 });
