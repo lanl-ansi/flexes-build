632f9fa6cb751c3d348338d40e554a8f28edf3df
James Arnold <arnold_j@lanl.gov>
2016-11-29 10:29:24 -0700

    Add more tests

--- test/test_basic.py
+++ test/test_basic.py
@@ -2,9 +2,26 @@ import os, pytest, sys
 
 sys.path.append('.')
 
-from flask import url_for
+import app
+import mock
+from flask import url_for, jsonify
 
 @pytest.mark.usefixtures('client_class')
 class TestEndpoints:
     def test_index(self):
         assert(self.client.get(url_for('index')).status_code == 200)
+
+    def test_service_get(self):
+        service_url = url_for('post_job', service='popecon')
+        assert(self.client.get(service_url).status_code == 200)
+
+    def test_bad_service_get(self):
+        service_url = url_for('post_job', service='foo')
+        assert(self.client.get(service_url).status_code == 500)
+
+    @mock.patch('app.submit_job', return_value='job_id')
+    def test_service_post(self, mock_submit):
+        expected = {'jobId': 'job_id', 'status': 'submitted'}
+        service_url = url_for('post_job', service='test')
+        resp = self.client.post(service_url)
+        assert(resp.json == expected)
