3dd67eca9db0b15020dbaafe952c5396aaa62d75
James Arnold <arnold_j@lanl.gov>
2016-11-30 07:05:09 -0700

    Condense deploy script

--- deploy.py
+++ deploy.py
@@ -2,11 +2,8 @@ import subprocess
 from git import Repo
 
 def deploy():
-    repo = Repo('.')
-    origin = repo.remotes.origin
-    origin.pull()
+    Repo('.').remotes.origin.pull()
     subprocess.call(['touch', 'production.wsgi'])
-
 
 if __name__ == '__main__':
     deploy()
