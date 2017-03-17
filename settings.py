7af2473f2d1f1a92fc0ed832c0729a5c77ec5070
Carleton <cjc@lanl.gov>
2017-03-17 14:07:15 -0600

    cleaning up test command (#45)

--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ STATUS_FAIL = 'failed'
 DOCKER_WORKER_TYPE = 'docker'
 DOCKER_REGISTRY = 'hub.lanlytics.com'
 
-REDIS_HOST = 'jobs.be6b1p.0001.usgw1.cache.amazonaws.com'
+REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
