9888947862e245fe525412acf0109978b4b868a1
James Arnold <arnold_j@lanl.gov>
2017-08-15 18:07:18 -0600

    Remove deploy script

--- deploy.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from git import Repo
-
-def deploy():
-    Repo('.').remotes.origin.pull()
-    subprocess.call(['touch', 'production.wsgi'])
-
-if __name__ == '__main__':
-    deploy()
