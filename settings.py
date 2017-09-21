2439da99555c25e0a152b5f19d0ced3fc2daea2a
James Arnold <arnold_j@lanl.gov>
2017-09-21 10:23:26 -0600

    Restore DOCKER_WORKER_TYPE to settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ STATUS_ACTIVE = 'active'
 STATUS_RUNNING = 'running'
 STATUS_FAIL = 'failed'
 
+DOCKER_WORKER_TYPE = 'docker'
 DOCKER_REGISTRY = 'hub.lanlytics.com'
 
 REDIS_HOST = 'jobs.be6b1p.0001.usgw1.cache.amazonaws.com'
