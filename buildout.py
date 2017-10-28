14251eb17e6c1d20714b4d75a3f56e9077123c99
James Arnold <arnold_j@lanl.gov>
2017-10-28 16:19:13 -0600

    Rough out deploy functions

--- buildout.py
+++ buildout.py
@@ -2,9 +2,12 @@ import boto3
 import time
 
 class Instance:
-    def __init__(self, ip, pem_file, user='ec2-user'):
-        self.address = '{}@{}'.format(user, ip)
-        self.pem_file = pem_file
+    def __init__(self, instance_id, user='ec2-user'):
+        ec2 = boto3.resource('ec2')
+        instance = ec2.Instance(instance_id)
+        public_ip = instance.public_ip_address
+        self.address = '{}@{}'.format(user, public_ip)
+        self.pem_file = '~/.ssh/{}.pem'.format(instance.key_name)
 
     def ssh(self, command):
         return subprocess.check_output([
@@ -57,18 +60,59 @@ def create_stack(**kwargs):
     return stack.outputs
 
 
-def deploy_registry(registry_instance):
-    # Copy build file to instance
-    # Run build file
-    # Launch docker-compose application
+def deploy_registry(registry_instance, registry_bucket):
+    print('Building docker registry')
+    remote_dest = '~/docker-registry/'
+    files = ['docker-compose.yml', 
+             'config.yml', 
+             'requirements.txt', 
+             'create_config.py',
+             'nginx.conf']
+    print('Copying files')
+    registry_instance.scp(' '.join(files), remote_dest)
+    print('Installing Python packages')
+    registry_instance.ssh('pip install -r ~/docker-registry/requirements.txt')
+    print('Creating configuration')
+    registry_instance.ssh('cd docker-registry && ./create_config.py {}'.format(registry_bucket))
+    print('Launching application')
+    registry_instance.ssh('cd docker-registry && docker-compose up -d --force-recreate')
 
 
 def deploy_api_server(api_instance):
-    api_server = Instance(api_server_id)
-    # 
+    print('Building API server')
+    remote_dest = '~/lanlytics-api/'
+    files = ['deploy-requirements.txt',
+             'create_compose.py',
+             'nginx.conf',
+             'lanlytics-api.tar']
+    print('Copying files')
+    api_instance.ssh('mkdir lanlytics-api')
+    api_instance.scp(' '.join(files), remote_dest)
+    print('Loading Docker image')
+    api_instance.ssh('docker load -i ~/lanlytics-api/lanlytics-api.tar')
+    print('Installing Python packages')
+    api_instance.ssh('pip install -r ~/lanlytics-api/deploy-requirements.txt')
+    print('Creating configuration')
+    api_instance.ssh('cd lanlytics-api && ./create_compose.py')
+    print('Launching application')
+    api_instance.ssh('cd lanlytics-api && docker-compose up -d --force-recreate')
 
 
-def deploy_worker(worker_instance)
+def deploy_worker(worker_instance):
+    print('Building API worker')
+    api_instance.ssh('mkdir lanlytics-api-worker')
+    remote_dest = '~/lanlytics-api-worker/'
+    print('Copying files')
+    worker_instance.scp('lanlytics-api-worker.tar', remote_dest)
+    print('Loading Docker image')
+    worker_instance.ssh('docker load -i ~/lanlytics-api-worker/lanlytics-api-worker.tar')
+    print('Launching worker')
+    worker_instance.ssh('sudo yum install jq -y')
+    worker_instance.ssh('docker run -d \
+            -e AWS_DEFAULT_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region) \
+            -v /home/ec2-user:/home/ec2-user \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            lanlytics-api-worker docker')
 
 
 def create_worker_ami():
@@ -81,8 +125,8 @@ def buildout_api():
     worker = Instance(outputs[2]['OutputValue'])
     redis_endpoint = outputs[3]['OutputValue']
 
-    deploy_registry(regsitry)
-    deploy_api_server(api_server)
-    deploy_worker(worker)
+    deploy_registry(Instance(regsitry))
+    deploy_api_server(Instance(api_server))
+    deploy_worker(Instance(worker))
 
     print('All done; go home')
