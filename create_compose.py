f564fd88a7d395798a65aa6dd7f1c632bba4645d
James Arnold <arnold_j@lanl.gov>
2017-10-28 10:16:22 -0600

    Dynamically create compose configuration

---
+++ create_compose.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import requests
+import yaml
+
+def create_compose():
+    with open('docker-compose.yml') as f:
+        compose = yaml.load(f)
+
+#    resp = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document')
+#    region = resp.json()['region']
+    region = 'us-gov-west-1'
+
+    compose['services']['flask']['environment'] = ['AWS_DEFAULT_REGION={}'.format(region)]
+
+    with open('docker-compose.yml', 'w') as f:
+        yaml.dump(compose, f, default_flow_style=False)
+
+if __name__ == '__main__':
+    create_compose()
