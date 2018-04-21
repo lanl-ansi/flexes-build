0c5aff34e1218fd02036dcc83cfbcbd76d56ff98
James Arnold <arnold_j@lanl.gov>
2018-04-21 11:27:21 -0600

    Stub out retrieval of API state

--- utils.py
+++ utils.py
@@ -7,13 +7,19 @@ from aiohttp import ClientSession
 from settings import *
 from uuid import uuid4
 
+JOB_PREFIX = 'job:'
+MESSAGE_PREFIX = 'messages:'
+QUEUE_PREFIX = 'queue:'
+WORKER_PREFIX = 'worker:'
+
+
 def submit_job(db, message):
     job_id = str(uuid4())
     message['job_id'] = job_id
     message['status'] = 'submitted'
     queue = message['queue'] if 'queue' in message.keys() else 'docker'
 
-    job = 'job:{}'.format(job_id)
+    job = JOB_PREFIX + job_id
     # Create job db entry
     db.hmset(job, message)
     # Push to queue
@@ -23,8 +29,8 @@ def submit_job(db, message):
 
 
 def query_job_status(db, job_id):
-    job = 'job:{}'.format(job_id)
-    status = db.hget(job, 'job_id')
+    job = JOB_PREFIX + job_id
+    status = db.hget(job, 'status')
     if status is not None:
         return {'job_id': job_id, 'status': status.decode()}
     else:
@@ -32,7 +38,7 @@ def query_job_status(db, job_id):
 
 
 def get_job_result(db, job_id):
-    job = 'job:{}'.format(job_id)
+    job = JOB_PREFIX + job_id
     result = db.hgetall(job)
     if result is not None:
         return result
@@ -43,11 +49,39 @@ def get_job_result(db, job_id):
         return response['Item']
 
 
-def all_jobs():
+def parse_hashmap(db, name, keys):
+    try:
+        return json.loads(db.hmget(name, keys).decode())
+    except Exception as e:
+        return None
+
+
+def job_messages(db, job_id):
+    job = MESSAGE_PREFIX + job_id
+    messages = db.get(job)
+    return json.loads(messages.decode()) if messages is not None else []
+
+
+def all_running_jobs(db):
+    jobs = [parse_hashmap(db, job, ['job_id', 'status', 'queue']) 
+            for job in db.keys(pattern=JOB_PREFIX)]
     dyn = boto3.resource('dynamodb', endpoint_url=DYNAMODB_ENDPOINT)
     table = dyn.Table(TABLE_NAME)
     response = table.scan(Select='ALL_ATTRIBUTES')
     return response['Items']
+
+
+def all_queues(db):
+    queue_names = [queue for queue in db.keys(pattern=QUEUE_PREFIX)]
+    queues = [{'name': queue, 'jobs': db.slen(queue)} for queue in queue_names] 
+    return queues
+
+
+def all_workers(db):
+    worker_ids = [worker for worker in db.keys(pattern=WORKER_PREFIX)]
+    workers = [parse_worker(db, worker) for worker in worker_ids]
+    workers = [json.loads(worker.decode()) for worker in db.get(match=WORKER_PREFIX)]
+    return workers
 
 
 def list_services(tags=None):
