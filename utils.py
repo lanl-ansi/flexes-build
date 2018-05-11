b101f0a352e125449fc19a650cddbf335f3c3eb9
James Arnold <arnold_j@lanl.gov>
2018-05-11 16:37:51 -0600

    Remove redundant functions

--- utils.py
+++ utils.py
@@ -63,47 +63,6 @@ def get_instance_info():
     return instance_id, instance_type, private_ip
 
 
-# Database
-def receive_message(db, queue):
-    message = db.rpop(queue)
-    if message is not None:
-        message = json.loads(message)
-        update_job(db, message['job_id'], config['STATUS_RUNNING'])
-    return message
-
-
-def update_job(db, job_id, status, result=None, stdout_data=None, stderr_data=None):
-    job = 'job:{}'.format(job_id)
-    queue = db.hget(job, 'queue')
-    db.hmset(job, 
-            {'status': status, 
-             'result': result, 
-             'stdout': stdout_data, 
-             'stderr': stderr_data})
-    if status == config['STATUS_RUNNING']:
-        db.sadd('{}:jobs:running'.format(queue), job_id)
-    elif status in [config['STATUS_COMPLETE'], config['STATUS_FAIL']]:
-        db.expire(job, 60)
-        db.srem('{}:jobs'.format(queue), job_id)
-        db.srem('{}:jobs:running'.format(queue), job_id)
-        dyn = boto3.resource('dynamodb', endpoint_url=config['DYNAMODB_ENDPOINT'])
-        table = dyn.Table(config['JOBS_TABLE'])
-        table.update_item(Key={'job_id': job_id},
-                          UpdateExpression='SET #stat = :val1, #r = :val2',
-                          ExpressionAttributeNames={'#stat': 'status', '#r': 'result'},
-                          ExpressionAttributeValues={':val1': status, ':val2': result})
-    elif status == config['STATUS_ACTIVE']:
-        db.expire(job, 30)
-        db.srem('{}:jobs'.format(queue), job_id)
-        db.srem('{}:jobs:running'.format(queue), job_id)
-    return status, result
-
-
-def update_job_messages(db, job_id, messages):
-    job = 'job:{}'.format(job_id)
-    db.hmset(job, {'messages': messages})
-
-
 # Validation
 def is_str_list(x):
     return (isinstance(x, list) and all(isinstance(s, str) for s in x))
@@ -123,18 +82,3 @@ def isvalid(obj, schema):
         return True
     except ValidationError:
         return False
-
-
-def image_exists(image_name, tag='latest'):
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock', version='auto')
-    image = '{}/{}:{}'.format(config['DOCKER_REGISTRY'], image_name, tag)
-    try:
-        image = client.images.get(image)
-        return True
-    except docker.errors.ImageNotFound:
-        try:
-            print('Image {} not found locally'.format(image))
-            client.images.pull(image)
-            return True
-        except docker.errors.ImageNotFound:
-            return False
