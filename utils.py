0404a8f7f052ca2f33d9faac0b8590d451ab746c
arnold_j <arnold_j@lanl.gov>
2018-07-18 15:33:50 -0600

    Add documentation

--- utils.py
+++ utils.py
@@ -10,6 +10,16 @@ from uuid import uuid4
 config = load_config()
 
 def submit_job(db, message):
+    '''Submit a job to the Redis queue.
+    
+    Args:
+        db (redis.StrictRedis): A Redis database connection.
+        message (dict): A message containing execution information.
+            Must conform to message_schema.json
+
+    Returns:
+        str: The unique ID for the submitted job
+    '''
     job_id = str(uuid4())
     message['job_id'] = job_id
     message['status'] = 'submitted'
@@ -25,6 +35,16 @@ def submit_job(db, message):
 
 
 def query_job_status(db, job_id):
+    '''Query the status of a job
+
+    Args:
+        db (redis.StrictRedis): A Redis database connection.
+        job_id (str): The unique ID for the submitted job.
+    
+    Returns:
+        dict: A dictionary with job information including a "status" 
+            key with the current status of the job.
+    '''
     job = config['JOB_PREFIX'] + job_id
     status = db.hget(job, 'status')
     if status is not None:
@@ -34,6 +54,15 @@ def query_job_status(db, job_id):
 
 
 def get_job_result(db, job_id):
+    '''Query the status of a job
+
+    Args:
+        db (redis.StrictRedis): A Redis database connection.
+        job_id (str): The unique ID for the submitted job.
+    
+    Returns:
+        dict: A dictionary with all job information.
+    '''
     job = config['JOB_PREFIX'] + job_id
     result = db.hgetall(job)
     if result != {}:
@@ -46,6 +75,16 @@ def get_job_result(db, job_id):
 
 
 def parse_hashmap(db, name, keys):
+    '''Retrieve a set of keys from a Redis hashmap
+
+    Args:
+        db (redis.StrictRedis): A Redis database connection.
+        name (str): The key for the hashmap in the Redis database.
+        keys (list): A list of keys to retrieve from the hashmap.
+
+    Returns:
+        dict: A dictionary containing the keys and values in the hashmap.
+    '''
     try:
         return dict(zip(keys, db.hmget(name, keys)))
     except Exception as e:
@@ -53,24 +92,60 @@ def parse_hashmap(db, name, keys):
 
 
 def job_messages(db, job_id):
+    '''Retrieve all of the intermediate messages produced by a job.
+
+    Args:
+        db (redis.StrictRedis): A Redis database connection.
+        job_id (str): The unique ID for the submitted job.
+
+    Returns:
+        list: The intermediate messages produced by the job.
+    '''
     job = config['MESSAGE_PREFIX'] + job_id
     messages = db.get(job)
     return json.loads(messages) if messages is not None else []
 
 
 def all_running_jobs(db):
+    '''List all running jobs
+
+    Args:
+        db (redis.StrictRedis): A Redis database connection.
+
+    Returns:
+        list: A list of jobs as dictionaries containing the 
+            job_id, status and queue for each job.
+    '''
     jobs = [parse_hashmap(db, job, ['job_id', 'status', 'queue']) 
             for job in db.keys(pattern='{}*'.format(config['JOB_PREFIX']))]
     return jobs
 
 
 def all_queues(db):
+    '''List all queues with active jobs
+
+    Args:
+        db (redis.StrictRedis): A Redis database connection.
+
+    Returns:
+        list: A list of active queues as dictionaries containing the 
+            name and number of jobs in each queue.
+    '''
     queues = [{'name': queue.replace(config['QUEUE_PREFIX'], ''), 'jobs': db.scard(queue)} 
               for queue in db.keys(pattern='{}*'.format(config['QUEUE_PREFIX']))] 
     return queues
 
 
 def all_workers(db):
+    '''List all workers
+
+    Args:
+        db (redis.StrictRedis): A Redis database connection.
+
+    Returns:
+        list: A list of workers as dictionaries containing the 
+            id, status and queue for each worker.
+    '''
     workers = []
     for worker in db.keys(pattern='{}*'.format(config['WORKER_PREFIX'])):
         worker_id = worker.replace(config['WORKER_PREFIX'], '')
@@ -79,6 +154,27 @@ def all_workers(db):
 
 
 def list_services(tags=None):
+    '''List all available services deployed through the API
+
+    Args:
+        tags (list, optional): A list of tags to filter the results on (e.g., latest).
+
+    Returns:
+        dict: A list of available services and tags
+
+        {
+            'services': [
+                {
+                    'name': 'serviceA'
+                    'tags': ['latest']
+                },
+                {
+                    'name': 'serviceB',
+                    'tags': ['latest', 'dev']
+                }
+            ]
+        }
+    '''
     loop = asyncio.get_event_loop()
     responses = loop.run_until_complete(get_services())
 
@@ -95,6 +191,16 @@ def list_services(tags=None):
 
 
 async def get_services():
+    '''Retrieve all of the images and their tags contained in the Docker registry
+
+    Returns:
+        list: A list of responses from the Docker registry
+
+        [
+            {'name': 'serviceA', 'tags': ['latest']},
+            {'name': 'serviceB', 'tags': ['latest', dev]}
+        ]
+    '''
     async with ClientSession() as session:
         url = 'https://{}/v2/_catalog'.format(config['DOCKER_REGISTRY'])
         services = await fetch(session, url)
@@ -108,5 +214,14 @@ async def get_services():
 
 
 async def fetch(session, url):
+    '''Wrapper for aiohttp GET request
+    
+    Args:
+        session (aiohttp.ClientSession): HTTP session
+        url (str): URL to fetch
+
+    Returns:
+        coroutine: The request response in JSON format
+    '''
     async with session.get(url) as response:
         return await response.json()
