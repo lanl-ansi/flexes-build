b8b9f57ef9babd052a201ab6a24b3ce34a0fb738
James Arnold <arnold_j@lanl.gov>
2019-03-21 13:47:43 -0600

    Fix config loader

--- flexes_build/config.py
+++ flexes_build/config.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 PACKAGE_DIR = Path(__file__).resolve().parent
 
-def load_config(config_path='config.json'):
+def load_config(config_path=None):
     default = PACKAGE_DIR.joinpath('default_config.json')
     with default.open() as f:
         config = json.load(f)
 
-    user_config = PACKAGE_DIR.joinpath(config_path)
+    user_config = Path(config_path) if config_path is not None else PACKAGE_DIR.joinpath('config.json')
     if user_config.exists():
         with user_config.open() as f:
             user = json.load(f)
@@ -19,8 +19,8 @@ def load_config(config_path='config.json'):
     return config
 
 
-def load_message_schema(message_schema_path='message_schema.json'):
-    message_schema = PACKAGE_DIR.joinpath(message_schema_path)
+def load_message_schema(message_schema_path=None):
+    message_schema = Path(message_schema_path) if message_schema_path is not None else PACKAGE_DIR.joinpath('message_schema.json')
     with message_schema.open() as f:
         schema = json.load(f)
     return schema
